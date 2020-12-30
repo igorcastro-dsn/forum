2372a835320b24fc243c443cb23a26053dee873d
igorcastro-dsn <igorcastro.dsn@gmail.com>
2020-12-30 12:32:21 -0200

    ADD primeiro endpoint de topicos

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+
+	@GetMapping("/topicos")
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida", "Dúvida com spring", new Curso("Spring", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	}
+	
+}
