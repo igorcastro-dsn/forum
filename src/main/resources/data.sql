7c9b74a94f76c1403b5ab0207bcc6d87a3afd583
igorcastro-dsn <igorcastro.dsn@gmail.com>
2021-01-01 23:11:35 -0200

    ADD autenticação

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$wa20e6.txRv19acMXX2aa.Wus.rd3pibedqGCWP4q9lmWnVKsN5gG');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
